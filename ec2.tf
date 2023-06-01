045859d94eda101aefc42794e18ee4ea2e9d51aa
Richard Oppong <richgoldd2@gmail.com>
2023-06-01 17:59:34 -0400

    modified ec2.tf file

--- ec2.tf
+++ ec2.tf
@@ -42,11 +42,12 @@ resource "aws_instance" "Jenkins_server" {
 
   provisioner "remote-exec"  {
     inline  = [
-      "sudo yum install -y jenkins java-11-openjdk-devel",
+      "sudo yum update –y",
       "sudo yum -y install wget",
       "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
       "sudo yum upgrade -y",
+      "sudo amazon-linux-extras install java-openjdk11 -y",
       "sudo yum install jenkins -y",
       "sudo systemctl enable jenkins",
       "sudo systemctl start jenkins",
