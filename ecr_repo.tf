739f565f3c01184c2406b086fbfe83117ecc34db
Richard Oppong <richgoldd@yahoo.com>
2023-06-02 10:59:42 +0000

    modified ecr-repo.tf file

--- ecr_repo.tf
+++ ecr_repo.tf
@@ -8,3 +8,13 @@ resource "aws_ecr_repository" "apprepo" {
     scan_on_push = false
   }
 }
+
+resource "aws_ecr_repository" "apprepo1" {
+  name                 = "docker-demo-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
