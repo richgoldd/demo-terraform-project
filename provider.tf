7e1b0b3503505958431cd3ede151ca3e19b34a0a
Richard Boakye <richgoldd@yahoo.com>
2023-06-02 23:48:21 -0400

    modified Jenkinsfile

--- provider.tf
+++ provider.tf
@@ -2,13 +2,13 @@
 
 #Note: Ensure to disable backend resource before applying to first register local tfstate file. Afterwards enable for remote tfstate setup
 terraform {
-  backend "s3" {
-    bucket         = "preprod-environment-tfstate" 
-    key            = "preprod-environment/terraform.tfstate"
-    region         = "us-west-1"
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "preprod-environment-tfstate" 
+  #   key            = "preprod-environment/terraform.tfstate"
+  #   region         = "us-west-1"
+  #   dynamodb_table = "terraform-state-locking"
+  #   encrypt        = true
+  # }
 
   required_providers {
     aws = {
@@ -23,36 +23,36 @@ provider "aws" {
   profile = "terra"
 }
 
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket        = "preprod-environment-tfstate"
-#   force_destroy = true
-# }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "preprod-environment-tfstate"
+  force_destroy = true
+}
 
-# resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-#   bucket = aws_s3_bucket.terraform_state.bucket
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "terraform-state-locking"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
 
 
